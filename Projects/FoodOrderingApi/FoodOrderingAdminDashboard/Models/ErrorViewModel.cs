e14545d74bfe5d0f2521f9c1c2a0ff6cc279b8c1
ankitsharma1994 <92724367+ankitsharma1994@users.noreply.github.com>
2023-11-27 15:27:11 +0530

    Delete Projects directory

--- Projects/FoodOrderingApi/FoodOrderingAdminDashboard/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FoodOrderingAdminDashboard.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
